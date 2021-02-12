cd708e09b0311353251ce317d2a70f6869288113
josejhonata <josetrevisan04@gmail.com>
2021-02-11 23:57:05 -0300

    Criando migrations e models para o banco de dados

--- security/database/migrations/2014_10_12_000000_create_users_table.php
+++ security/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('cpf');
+            $table->string('numero_da_carteira');
+            $table->string('cep');
+            $table->string('tipo');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
