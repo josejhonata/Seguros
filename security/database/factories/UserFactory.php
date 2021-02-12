502be4fa570e25e36547abe8382e42dc44a703f5
josejhonata <josetrevisan04@gmail.com>
2021-02-12 02:45:01 -0300

    adicionando o factories com a biblioteca faker da tabala user

--- security/database/factories/UserFactory.php
+++ security/database/factories/UserFactory.php
@@ -24,6 +24,9 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name,
+            'cpf' => $this->faker->numerify('###.###.###-##'),
+            'numero_da_carteira' => $this->faker->numerify('###########'),
+            'cep'=> $this->faker->bothify('?????-###'),
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
